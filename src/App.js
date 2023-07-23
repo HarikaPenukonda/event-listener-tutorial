29e6f20b078d6ec989d365cbecdf13d38c243b5e
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-23 15:42:05 -0500

    event listener

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
 
 export default function App(){
+    function handleClick(){
+        //console.log("I was clicked")
+        return(
+            <h1>You are clicked</h1>
+        )
+    }
+    /*
+        Add function - handleClick() to the button
+    */
     return(
         <div className="container">
-            
+            <img 
+                src="./images/i1.jpeg"
+                width="200px"
+                />
+            <br></br>
+            <button onClick={handleClick}>Click me</button>
         </div>
     )
 }
+
