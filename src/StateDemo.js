2a2e51fb962b6e32346405af947908384972bb50
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-24 15:55:34 -0500

    changing state

--- src/StateDemo.js
+++ src/StateDemo.js
@@ -5,12 +5,20 @@ export  default function StateDemo(){
         Challenge: Replace our hard-coded "Yes" on the page with
         some state initiated with React.useState()
     */
-   const [result,func] = React.useState("Yes")
+   const [isImportant,setIsImportant] = React.useState("Yes")
+   /*
+    Challenge:
+        1. Create a function called `handleClick` that runs setIsImportant("no")
+        2. add a click even listener to the div.state--value that runs `handleClick` when the div is clicked
+   */
+    function handleClick(){
+        setIsImportant("No")
+    }
     return(
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
-            <div className="state--value">
-                <h1>{result[0]}</h1>
+            <div onClick={handleClick} className="state--value">
+                <h1>{isImportant}</h1>
             </div>
 
         </div>
