107d74ff1806e3745c7908d3b888657070829969
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-24 14:29:03 -0500

    Explanation

--- src/Conundrum.js
+++ src/Conundrum.js
@@ -8,7 +8,8 @@ export default function Conundrum(){
         of the "Thing" being added. Also, have your event
         listerner console.log() after adding the new item to the array
     */    
-    const thingsElement = thingsArray.map(items=><p key={items}>{items}</p>)
+    const thingsElement = thingsArray.map(items=><p key={items}>{items}</p>) 
+    // this line will run only one time and that's the very first time the app component gets rendered
     
 
     function addItem(){
@@ -16,7 +17,12 @@ export default function Conundrum(){
         thingsArray.push(newThingText)
         console.log(thingsArray);
     }
-    
+    /*
+        At this point the array is changing but we do not see this on screen
+        React is not looking at local arrays that are just saved within a componenet
+        to determine whether or not something should get re-rendered or rather if the return here
+        should run again with an updated value for `thingsElement`
+    */
     return(
         <div>
             <button onClick={addItem}>Add Items</button>
