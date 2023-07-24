8f9013680013d6b5c0aad17f31fd58992b6aed9c
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-24 16:30:12 -0500

    callback function notes

--- src/Counter.js
+++ src/Counter.js
@@ -6,9 +6,13 @@ import React from "react";
 
 export default function Counter(){
     const [count,setCount] = React.useState(0)
+    /*
+        Note : if you ever need the old value of state to help you determine the new value of state,
+        you should pass a callback function to your state setter function(setCount) instead of using state directly
+        This callback function will recieve the old value of state as its parameter, which you can use to determine your new state value
+    */
     function add(){
         // provide a callback function
-        //setCount(oldValue => oldValue + 1)
         setCount(prevCount => prevCount + 1)
     }
 
