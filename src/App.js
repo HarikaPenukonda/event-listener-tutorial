919d6eb4957778bd0ac0083d23abeaa7eae8dbd5
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-24 14:06:53 -0500

    Map over the thingsArray to generate a <p> element

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import React from "react";
+import Conundrum from "./Conundrum";
 
 export default function App(){
-    function handleClick(){
-        //console.log("I was clicked")
-        return(
-            <h1>You are clicked</h1>
-        )
-    }
-    /*
-        Add function - handleClick() to the button
-    */
     return(
-        <div className="container">
-            <img 
-                src="./images/i1.jpeg"
-                width="200px"
-                />
-            <br></br>
-            <button onClick={handleClick}>Click me</button>
-        </div>
+        <Conundrum/>
     )
 }
+
+// function handleClick(){
+//     //console.log("I was clicked")
+//     return(
+//         <h1>You are clicked</h1>
+//     )
+// }
+// /*
+//     Add function - handleClick() to the button
+// */
+// return(
+//     <div className="container">
+//         <img 
+//             src="./images/i1.jpeg"
+//             width="200px"
+//             />
+//         <br></br>
+//         <button onClick={handleClick}>Click me</button>
+//     </div>
+// )
 
---
+++ src/Conundrum.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Conundrum(){
+    const thingsArray = ["Thing 1", "Thing 2"]
+
+    /*
+        Challenge: Map over the thingsArray to generate
+        a <p> element for each item and render them on the page
+        below the button
+    */ 
+    const thingsElement = thingsArray.map(items=><p>{items}</p>)
+    return(
+        <div>
+            <button>Add Items</button>
+            {thingsElement}  
+        </div>
+    )
+}
