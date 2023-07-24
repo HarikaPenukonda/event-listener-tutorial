dd6cda6db0cad4e0e6308d816ce0a930420c6ae9
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-24 15:51:03 -0500

    UseState Array destructing

--- src/StateDemo.js
+++ src/StateDemo.js
@@ -5,12 +5,12 @@ export  default function StateDemo(){
         Challenge: Replace our hard-coded "Yes" on the page with
         some state initiated with React.useState()
     */
-   const result = React.useState("Yes")
+   const [result,func] = React.useState("Yes")
     return(
         <div className="state">
             <h1 className="state--title">Is state important to know?</h1>
             <div className="state--value">
-                <h1>{result}</h1>
+                <h1>{result[0]}</h1>
             </div>
 
         </div>
