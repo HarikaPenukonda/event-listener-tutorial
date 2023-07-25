c98f55fa5e8bb1b54f3bfccd1ea726ed26cf5148
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-25 13:41:14 -0500

    challenge 3 - flipping state

--- src/Ternary.js
+++ src/Ternary.js
@@ -8,14 +8,22 @@ export default function Ternary(){
 
         Challenge 2 : move our ternary operator inside of the JSX
         so the "Yes" and "No" are determined inside the <h1>
+
+        Challange 3 : 
+            - Initalize state for `isGoingOut` as a boolean
+            - Make it so clicking on the div state flips the boolean value from true to false viceversa
+            - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
     */
 
-    const isGoingOut = true
+    const [isGoingOut, setIsGoingOut] = React.useState(true)
     
+    function handleClick(){
+        setIsGoingOut(prevState => !prevState)
+    }
 
     return(
         <div>
-            <h1>Do I feel like going out tonight?</h1>
+            <h1 onClick={handleClick}>Do I feel like going out tonight?</h1>
             <h1>{isGoingOut ? "Yes" : "No"}</h1>
         </div>
     )
