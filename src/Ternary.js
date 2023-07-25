2dafde3a0abf020084f6a41c59376222f6e7baf7
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-25 13:13:59 -0500

    using handleClick function

--- src/Ternary.js
+++ src/Ternary.js
@@ -7,16 +7,22 @@ export default function Ternary(){
         - Make it so clicking the div.state--value flips that boolean value (true->false, false->true)
         - Display "Yes" if `isGoingOut` is true and No otherwise
     */
-    const [isGoingOut,setIsGoingOut] = React.useState(false)
+    const [isGoingOut,setIsGoingOut] = React.useState(true)
 
-    function handleChange(){
-        return setIsGoingOut(isGoingOut ? "Yes" : "No")
+    // one way
+    // function handleClick(){
+    //     setIsGoingOut(prevState => prevState ? "Yes" : "No")
+    // }
+
+    // another way
+    function handleClick(){
+        setIsGoingOut(prevState => !prevState)
     }
-    // const isGoingOut = false
+
     return(
         <div>
-            <h1 onClick={handleChange}>Do I feel like going out tonight?</h1>
-            <h1>{isGoingOut}</h1>
+            <h1 onClick={handleClick}>Do I feel like going out tonight?</h1>
+            <h1>{isGoingOut ? "Yes" : "No"}</h1>
         </div>
     )
 }
