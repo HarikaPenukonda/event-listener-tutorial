bb088f2d2e9a5c55e348030c3156a89fc4c813fe
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-25 13:03:07 -0500

    flipping state back and forth-own defined function

--- src/Ternary.js
+++ src/Ternary.js
@@ -2,15 +2,21 @@ import React from "react";
 
 export default function Ternary(){
     /*
-        Challenge: move our ternary directly inside of the JSX
-        so the "Yes" and "No" are determined inside the <h1>
+        Challenge: 
+        - Initialize state for `isGoingOut` as a boolean
+        - Make it so clicking the div.state--value flips that boolean value (true->false, false->true)
+        - Display "Yes" if `isGoingOut` is true and No otherwise
     */
+    const [isGoingOut,setIsGoingOut] = React.useState(false)
 
-    const isGoingOut = false
+    function handleChange(){
+        return setIsGoingOut(isGoingOut ? "Yes" : "No")
+    }
+    // const isGoingOut = false
     return(
         <div>
-            <h1>Do I feel like going out tonight?</h1>
-            <h1>{isGoingOut ? "Yes" : "No"}</h1>
+            <h1 onClick={handleChange}>Do I feel like going out tonight?</h1>
+            <h1>{isGoingOut}</h1>
         </div>
     )
 }
