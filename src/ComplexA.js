dec6a407e7b679f2df7bea508d9be57875e3e9f5
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-25 16:28:58 -0500

    using array map method and spread operator

--- src/ComplexA.js
+++ src/ComplexA.js
@@ -6,10 +6,10 @@ export default function ComplexA(){
         Challenge: See if you can do it all by yourself
     */
 
-    const[infoThings, setInfoThings] = React.useState(["Things 1", "Things 2"])
+    const[infoThings, setInfoThings] = React.useState(["Thing 1", "Thing 2"])
 
     function handleClick(){
-        setInfoThings(prevItems => [...prevItems, `"Thing "${prevItems.length + 1}`]) // [Things 1, Things2, Things3..]
+        setInfoThings(prevItems => [...prevItems, `Thing ${prevItems.length + 1}`]) // [Things 1, Things2,]
     }
 
     const thingsElements = infoThings.map(things => <p>{things}</p>)
