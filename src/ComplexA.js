182d57dc97b5c1cd4ea0e3c2940d6059ca2a7937
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-25 16:27:32 -0500

    spread operato

--- src/ComplexA.js
+++ src/ComplexA.js
@@ -9,7 +9,7 @@ export default function ComplexA(){
     const[infoThings, setInfoThings] = React.useState(["Things 1", "Things 2"])
 
     function handleClick(){
-        setInfoThings(prevItems => [...prevItems, `"Thing "${prevItems.length + 1}`])
+        setInfoThings(prevItems => [...prevItems, `"Thing "${prevItems.length + 1}`]) // [Things 1, Things2, Things3..]
     }
 
     const thingsElements = infoThings.map(things => <p>{things}</p>)
