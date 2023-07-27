d94d6609249ed419664c27e2a012426f95b2d224
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-27 17:33:58 -0500

    challenge - own code

--- src/Boxes/Box.js
+++ src/Boxes/Box.js
@@ -1,14 +1,34 @@
 import React from "react";
 
 export default function Box(props){
+
+    /*
+        Challenge : Create state controlling whether box is "on" or "off".
+        Use the incoming `props.on1 to determine the inital state.
+
+        Create an event listener so when the box is clicked, it
+        toggles from "on" to "off"
+
+        Goal: clicking each box should toggle it on and off
+
+    */
+
+    const [light,setLight] = React.useState(props.on)
+
+    function handleClick(){
+        setLight(prevState => !prevState)
+    }
+    
     const styles = {
-        backgroundColor : props.on ? "#222222" : "none"
+        backgroundColor : light ? "#222222" : "none"
     }
     return(
         <div 
             className="box" 
             style={styles}
             alt="box"
+            onClick={handleClick}
             ></div>
+            
     )
 }
