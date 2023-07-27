3215295b772c0173f65ca5387a464982d39b14cc
HarikaPenukonda <penukondaharika@gmail.com>
2023-07-27 15:55:52 -0500

    passing data around

--- src/PassingData/MyApp.js
+++ src/PassingData/MyApp.js
@@ -3,10 +3,15 @@ import Body from "./Body";
 import Header from "./Header"
 
 export default function MyApp(){
+    const [user,setUser] = React.useState("Joe")
     return(
         <div>
-            <Header/>
-            <Body/>
+            <Header
+                user={user}
+            />
+            <Body
+                user={user}
+            />
         </div>
     )
 }
